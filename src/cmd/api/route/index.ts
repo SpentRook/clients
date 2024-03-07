aee48cc85c7abf545ed2e48315f211be85260ab7
Richard Lion Guevara Puerta <leonvilla6@outlook.com>
2024-03-07 14:22:27 -0500

    Add health route to API

--- src/cmd/api/route/index.ts
+++ src/cmd/api/route/index.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { clientRouter } from "./client";
+import { healthRouter } from "./health";
 
 export const routes = Router()
 
 routes.use("/clients", clientRouter)
+routes.use("/health", healthRouter)
