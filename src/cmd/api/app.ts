9f028f07f3db09df7345da709216e25f9daf4297
Richard Lion Guevara Puerta <leonvilla6@outlook.com>
2024-03-07 14:21:58 -0500

    Add CORS middleware and 404 handler to app.js

--- src/cmd/api/app.ts
+++ src/cmd/api/app.ts
@@ -1,11 +1,18 @@
 import express from "express";
 import { routes } from "./route";
+import cors from "./config/cors";
 
 const app = express()
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(cors)
+
 app.use("/api/v1", routes)
+app.use((req, res) => {
+    res.status(404).json({ message: "Not Found path: " + req.url});
+});
+
 
 export default app
