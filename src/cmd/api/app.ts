32ed3a80a43fc2c818777415a9cfbbde81d09b50
Richard Lion Guevara Puerta <leonvilla6@outlook.com>
2024-03-07 11:47:00 -0500

    Add API routes to app

--- src/cmd/api/app.ts
+++ src/cmd/api/app.ts
@@ -1,8 +1,11 @@
 import express from "express";
+import { routes } from "./route";
 
 const app = express()
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/v1", routes)
 
 export default app
