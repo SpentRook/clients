d0cc462d22e563053b6a9fa636cfb2395cac85e2
Richard Lion Guevara Puerta <leonvilla6@outlook.com>
2024-03-07 14:21:10 -0500

    Fix PORT variable type in main.ts

--- src/cmd/main.ts
+++ src/cmd/main.ts
@@ -1,6 +1,6 @@
 import app from "./api/app";
 
-const PORT = process.env.PORT ?? 8080;
+const PORT = process.env.PORT ?? "8080";
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
