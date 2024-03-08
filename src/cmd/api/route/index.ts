01cb169061afb6be092558a8143515d59e37dfd3
Richard Lion Guevara Puerta <leonvilla6@outlook.com>
2024-03-07 22:59:54 -0500

    Add Swagger tags to route index.ts

--- src/cmd/api/route/index.ts
+++ src/cmd/api/route/index.ts
@@ -4,5 +4,17 @@ import { healthRouter } from "./health";
 
 export const routes = Router()
 
+/**
+ * @swagger
+ *  tags:
+ *    name: Clients
+ *    description: Clients for bank
+ * 
+ * @swagger
+ * tags:
+ *  name: Health
+ *  description: Health check
+ */
+
 routes.use("/clients", clientRouter)
 routes.use("/health", healthRouter)
